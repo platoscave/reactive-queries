744826074f70a2bc0a0b7627fad4fae6cd1bd4ed
platoscave <platoscave60@gmail.com>
2026-07-15 15:49:24 +0200

    added main for wasm32 target

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,4 +1,42 @@
-// app/src/main.rs
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
     app::run();
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    use eframe::wasm_bindgen::JsCast as _;
+
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let document = web_sys::window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("the_canvas_id")
+            .expect("Failed to find the_canvas_id")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("the_canvas_id was not a HtmlCanvasElement");
+
+        let web_options = eframe::WebOptions::default();
+
+        let start_result = eframe::WebRunner::new()
+            .start(canvas, web_options, Box::new(|cc| Ok(Box::new(app::run_web(cc)))))
+            .await;
+
+        if let Some(loading_text) = document.get_element_by_id("loading_text") {
+            match start_result {
+                Ok(_) => loading_text.remove(),
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
+    });
 }
